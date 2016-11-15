9376f7f3534f250497e6e86428da5bd92a60a9cf
Sam <duo929@hotmail.com>
2016-11-15 10:05:19 +0100

    Add string trim helper functions

--- src/Angelscript/util/StringUtils.h
+++ src/Angelscript/util/StringUtils.h
@@ -1,7 +1,11 @@
 #ifndef ANGELSCRIPT_UTIL_STRINGUTILS_H
 #define ANGELSCRIPT_UTIL_STRINGUTILS_H
 
+#include <algorithm>
+#include <cctype>
 #include <cstring>
+#include <functional>
+#include <string>
 
 //Implemented in the as namespace to prevent collisions with other implementations.
 namespace as
@@ -51,5 +55,34 @@ struct EqualTo_C_String final
 	}
 };
 }
+
+/*
+*	This code is based on the following Stack Overflow answer: http://stackoverflow.com/a/217605
+*/
+// trim from start
+inline std::string& LTrim( std::string& s )
+{
+	s.erase( s.begin(), std::find_if( s.begin(), s.end(),
+									  std::not1( std::ptr_fun<int, int>( std::isspace ) ) ) );
+	return s;
+}
+
+// trim from end
+inline std::string& RTrim( std::string& s )
+{
+	s.erase( std::find_if( s.rbegin(), s.rend(),
+						   std::not1( std::ptr_fun<int, int>( std::isspace ) ) ).base(), s.end() );
+	return s;
+}
+
+// trim from both ends
+inline std::string& Trim( std::string& s )
+{
+	return LTrim( RTrim( s ) );
+}
+
+/*
+*	End based on code.
+*/
 
 #endif //ANGELSCRIPT_UTIL_STRINGUTILS_H

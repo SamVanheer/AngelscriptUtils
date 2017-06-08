c959f4f0f382f42bf3f0dd2c060bd408baed27ff
Sam <duo929@hotmail.com>
2017-06-08 20:05:16 +0200

    Make check_winxp_support CMake macro check only for MSVC targets

--- cmake/WinXPSupport.cmake
+++ cmake/WinXPSupport.cmake
@@ -3,14 +3,17 @@
 #	If targeting Windows XP (v*_xp toolset) and using an MSVC version of Visual Studio 2015 or newer, it is disabled.
 #	Must be called after starting a project, othewise the required variables will not have been defined yet.
 macro( check_winxp_support )
-	#Verify that we have the data we need.
-	if( NOT MSVC_VERSION OR NOT CMAKE_VS_PLATFORM_TOOLSET )
-		MESSAGE( FATAL_ERROR "Include WinXPSupport.cmake after starting a project" )
-	endif()
+	#Visual Studio only.
+	if( MSVC )
+		#Verify that we have the data we need.
+		if( NOT MSVC_VERSION OR NOT CMAKE_VS_PLATFORM_TOOLSET )
+			MESSAGE( FATAL_ERROR "Include WinXPSupport.cmake after starting a project" )
+		endif()
 
-	if( NOT "${MSVC_VERSION}" LESS 1900 AND CMAKE_VS_PLATFORM_TOOLSET MATCHES ".*_xp$" )
-		MESSAGE( STATUS "Disabling Thread-safe initialization for Windows XP support" )
-		#Disable thread-safe init so Windows XP users don't get crashes.
-		set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /Zc:threadSafeInit-" )
+		if( NOT "${MSVC_VERSION}" LESS 1900 AND CMAKE_VS_PLATFORM_TOOLSET MATCHES ".*_xp$" )
+			MESSAGE( STATUS "Disabling Thread-safe initialization for Windows XP support" )
+			#Disable thread-safe init so Windows XP users don't get crashes.
+			set( CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} /Zc:threadSafeInit-" )
+		endif()
 	endif()
 endmacro( check_winxp_support )

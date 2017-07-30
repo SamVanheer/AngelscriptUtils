51c2265c645cdaf5dca93c7b5de704a00036e477
Sam <duo929@hotmail.com>
2017-07-30 23:24:53 +0200

    Update CMake utility files to latest version, refactor global state into per-target settings, use built-in CMake feature for C++ version setting, remove unnecessary includes install for Angelscript lib

--- cmake/MSVCRuntime.cmake
+++ cmake/MSVCRuntime.cmake
@@ -1,43 +1,37 @@
-#
-#	This file defines a macro that configures the MSVC runtime.
-#	Set the variable MSVC_RUNTIME to "static" to force the use of a static runtime.
-#
+if( MSVCRUNTIME_INCLUDED )
+  return()
+endif()
+set( MSVCRUNTIME_INCLUDED true )
 
-macro(configure_msvc_runtime)
-  if(MSVC)
-    # Default to statically-linked runtime.
-    if("${MSVC_RUNTIME}" STREQUAL "")
-      set(MSVC_RUNTIME "static")
-    endif()
-    # Set compiler options.
-    set(variables
-      CMAKE_C_FLAGS_DEBUG
-      CMAKE_C_FLAGS_MINSIZEREL
-      CMAKE_C_FLAGS_RELEASE
-      CMAKE_C_FLAGS_RELWITHDEBINFO
-      CMAKE_CXX_FLAGS_DEBUG
-      CMAKE_CXX_FLAGS_MINSIZEREL
-      CMAKE_CXX_FLAGS_RELEASE
-      CMAKE_CXX_FLAGS_RELWITHDEBINFO
-    )
-    if(${MSVC_RUNTIME} STREQUAL "static")
-      message(STATUS
-        "MSVC -> forcing use of statically-linked runtime."
-      )
-      foreach(variable ${variables})
-        if(${variable} MATCHES "/MD")
-          string(REGEX REPLACE "/MD" "/MT" ${variable} "${${variable}}")
-        endif()
-      endforeach()
-    else()
-      message(STATUS
-        "MSVC -> forcing use of dynamically-linked runtime."
-      )
-      foreach(variable ${variables})
-        if(${variable} MATCHES "/MT")
-          string(REGEX REPLACE "/MT" "/MD" ${variable} "${${variable}}")
-        endif()
-      endforeach()
-    endif()
-  endif()
+#!	Configures the runtime for the given target
+#	Only one runtime can be provided
+#
+#	\arg:target_name Name of the target whose runtime to configure
+#	\flag:STATIC Whether to use the static runtime
+#	\flag:DYNAMIC Whether to use the dynamic runtime
+#
+macro( configure_msvc_runtime target_name )
+	cmake_parse_arguments( ARG "STATIC;DYNAMIC" "" "" ${ARGN} )
+	
+	if( ARG_UNPARSED_ARGUMENTS )
+		message( FATAL_ERROR "unrecognized arguments: ${ARG_UNPARSED_ARGUMENTS}" )
+	endif()
+	
+	if( ARG_STATIC AND ARG_DYNAMIC )
+		message( FATAL_ERROR "The STATIC and DYNAMIC runtimes are mutually exclusive" )
+	endif()
+	
+	if( MSVC )
+		# Set compiler options.
+		if( ARG_STATIC )
+			target_compile_options( ${target_name} PRIVATE $<$<CONFIG:DEBUG>:/MTd> )
+			target_compile_options( ${target_name} PRIVATE $<$<NOT:$<CONFIG:DEBUG>>:/MT> )
+		elseif( ARG_DYNAMIC )
+			target_compile_options( ${target_name} PRIVATE $<$<CONFIG:DEBUG>:/MDd> )
+			target_compile_options( ${target_name} PRIVATE $<$<NOT:$<CONFIG:DEBUG>>:/MD> )
+		else()
+			#Should never happen, but if more combinations are needed, this will cover edge cases.
+			message( FATAL_ERROR "Unknown runtime selected" )
+		endif()
+	endif()
 endmacro()
--- cmake/PDBUtils.cmake
+++ cmake/PDBUtils.cmake
@@ -1,5 +1,11 @@
-#	Sets PDB file names to match the binaries
-macro( set_pdb_names _project_name )
+if( PDBUTILS_INCLUDED )
+  return()
+endif()
+set( PDBUTILS_INCLUDED true )
+
+#!	Sets PDB file names to match the binaries
+#	\arg:target_name Name of the target to configure PDB names for
+macro( set_pdb_names target_name )
 	foreach( config IN LISTS CMAKE_CONFIGURATION_TYPES )
 			#TODO: figure out if there's a way to check if a config is debug - Solokiller
 			set( POSTFIX )
@@ -9,12 +15,14 @@ macro( set_pdb_names _project_name )
 			
 			string( TOUPPER ${config} PDB_POSTFIX )
 			
-			set_target_properties( ${_project_name} PROPERTIES COMPILE_PDB_NAME_${PDB_POSTFIX} "${_project_name}${POSTFIX}" )
+			set_target_properties( ${target_name} PROPERTIES COMPILE_PDB_NAME_${PDB_POSTFIX} "${target_name}${POSTFIX}" )
 		endforeach()
 endmacro( set_pdb_names )
 
-#	Installs PDB files for a given project
-macro( install_pdbs _project_name destination )
+#!	Installs PDB files for a given target
+#	\arg:target_name Name of the target to install PDB files for
+#	\arg:destination Destination to install files to
+macro( install_pdbs target_name destination )
 	if( WIN32 )
 		foreach( config IN LISTS CMAKE_CONFIGURATION_TYPES )
 			#TODO: figure out if there's a way to check if a config is debug - Solokiller
@@ -23,7 +31,7 @@ macro( install_pdbs _project_name destination )
 				set( POSTFIX ${CMAKE_DEBUG_POSTFIX} )
 			endif()
 			
-			install( FILES ${CMAKE_CURRENT_BINARY_DIR}/${config}/${_project_name}${POSTFIX}.pdb DESTINATION ${destination} CONFIGURATIONS ${config} )
+			install( FILES ${CMAKE_CURRENT_BINARY_DIR}/${config}/${target_name}${POSTFIX}.pdb DESTINATION ${destination} CONFIGURATIONS ${config} )
 		endforeach()
 	endif()
 endmacro( install_pdbs )

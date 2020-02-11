e443203cac825bd6d3108f0eeb9e6f20860fcd2e
Sam <duo929@hotmail.com>
2020-02-11 22:21:31 +0100

    Remove EXTERNAL_DIR & SRC_DIR variables, use file scope replacement for SCR_DIR, generate filters directly

--- cmake/InputFilesList.cmake
+++ cmake/InputFilesList.cmake
@@ -74,19 +74,6 @@ function( preprocess_sources )
 	set( PREP_SRCS ${PREP_SRCS} PARENT_SCOPE )
 endfunction( preprocess_sources )
 
-#!	Create the source groups for source files
-#	\arg:_src_root_path Root directory for the list of source files
-function( create_source_groups _src_root_path )
-	get_property( SRCS GLOBAL PROPERTY SRCS_LIST )
-	
-	foreach( _source IN ITEMS ${SRCS} )
-		get_filename_component( _source_path "${_source}" PATH )
-		file( RELATIVE_PATH _source_path_rel "${_src_root_path}" "${_source_path}" )
-		string( REPLACE "/" "\\" _group_path "${_source_path_rel}" )
-		source_group( "${_group_path}" FILES "${_source}" )
-	endforeach()
-endfunction( create_source_groups )
-
 #!	Function to clear the sources list
 #	Call once sources have been added to a target and source groups have been created
 function( clear_sources )

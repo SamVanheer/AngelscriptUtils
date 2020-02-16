5dbe6c15f61cff36ae93bb123bb67c686044117b
Sam <duo929@hotmail.com>
2020-02-16 21:50:45 +0100

    Use new parameter naming convention for type info functions

--- include/AngelscriptUtils/utility/TypeInfo.h
+++ include/AngelscriptUtils/utility/TypeInfo.h
@@ -8,49 +8,49 @@ namespace asutils
 *	@brief Returns whether the given type id is that of void
 *	Exists mostly for completeness
 */
-inline bool IsVoid(const int iTypeId)
+inline bool IsVoid(const int typeId)
 {
-	return asTYPEID_VOID == iTypeId;
+	return asTYPEID_VOID == typeId;
 }
 
 /**
 *	@brief Returns whether a given type id is that of a primitive type
 */
-inline bool IsPrimitive(const int iTypeId)
+inline bool IsPrimitive(const int typeId)
 {
-	return asTYPEID_BOOL <= iTypeId && iTypeId <= asTYPEID_DOUBLE;
+	return asTYPEID_BOOL <= typeId && typeId <= asTYPEID_DOUBLE;
 }
 
 /**
 *	@brief Returns whether a given type id is that of an enum type
 */
-inline bool IsEnum(const int iTypeId)
+inline bool IsEnum(const int typeId)
 {
-	return iTypeId > asTYPEID_DOUBLE && (iTypeId & asTYPEID_MASK_OBJECT) == 0;
+	return typeId > asTYPEID_DOUBLE && (typeId & asTYPEID_MASK_OBJECT) == 0;
 }
 
 /**
 *	@brief Returns whether a given type id is that of an integer type
 */
-inline bool IsInteger(const int iTypeId)
+inline bool IsInteger(const int typeId)
 {
-	return (iTypeId >= asTYPEID_INT8) && (iTypeId <= asTYPEID_UINT64);
+	return (typeId >= asTYPEID_INT8) && (typeId <= asTYPEID_UINT64);
 }
 
 /**
 *	@brief Returns whether a given type id is that of a float type
 */
-inline bool IsFloat(const int iTypeId)
+inline bool IsFloat(const int typeId)
 {
-	return (iTypeId >= asTYPEID_FLOAT) && (iTypeId <= asTYPEID_DOUBLE);
+	return (typeId >= asTYPEID_FLOAT) && (typeId <= asTYPEID_DOUBLE);
 }
 
 /**
 *	@brief Returns whether a given type id is that of an object type
 */
-inline bool IsObject(const int iTypeId)
+inline bool IsObject(const int typeId)
 {
-	return (iTypeId & asTYPEID_MASK_OBJECT) != 0;
+	return (typeId & asTYPEID_MASK_OBJECT) != 0;
 }
 
 /**
